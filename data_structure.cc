ea7ac702fb1a15c624efe89d638e31c0260d8bc5
Simone <SirSimonSays@users.noreply.github.com>
2016-12-19 19:19:19 +0100

    Delete data_structure.cc

--- data_structure.cc
+++
@@ -1,8 +0,0 @@
-/** this class contain the definitions of the variables shared from the other
-  * files. A sort of container of all global variables.
-  */
-
-#include "data_structure.h"
-
-const int default_x = 1270; /**<default dimension for the width of the windows*/
-const int default_y = 650;  /**<default dimension for the height of the windows*/
